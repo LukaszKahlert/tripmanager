393c7601ad495ad628e0fb22787288443fad20d3
LukaszKahlert <lukasz.kahlert@gmail.com>
2018-04-16 20:25:28 +0200

    test

--- src/main/java/pl/edu/agh/mwo/Trip.java
+++ src/main/java/pl/edu/agh/mwo/Trip.java
@@ -5,15 +5,44 @@ import java.util.Collection;
 
 public class Trip {
 
-	Collection<Photo> photos = new ArrayList<Photo>();
-	
+	private String name;
+	private String description;
+	private Collection<Photo> photos;
+
+	public Trip(String name, String description) {
+		if (name == null || description == null || name.equals("") || description.equals("")) {
+			throw new IllegalArgumentException();
+		}
+		this.name = name;
+		this.description = description;
+	}
+
+	public String name() {
+		return getName();
+	}
+
+	public String description() {
+		return getDescription();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
 	public Collection<Photo> getPhotos() {
 		return photos;
 	}
 
 	public void addPhoto(Photo photo) {
+
+		if (photos == null) {
+			photos = new ArrayList<>();
+		}
 		photos.add(photo);
-	
 	}
 
 }
---
+++ src/test/java/pl/edu/agh/mwo/PhotoTest.java
@@ -0,0 +1,30 @@
+package pl.edu.agh.mwo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PhotoTest {
+
+	
+
+	@Test
+	public void testPhotoCtorIsCorrect() {
+		Photo thePhoto = new Photo("Kraków-Wawel");
+		Assert.assertEquals("Kraków-Wawel", thePhoto.comment());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testPhotoWithNullComment() {
+		Photo thePhoto = new Photo(null);
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testPhotoWithEmptyComment() {
+		Photo thePhoto = new Photo("");
+
+	}
+
+}
